ae7a369a8636db32c5fd24d94468e31989ae9ed3
Fran1611 <francisco.ibanez@correo.ucu.edu.uy>
2020-04-26 09:15:39 -0300

    comentarios

--- src/Library/Iprinter.cs
+++ src/Library/Iprinter.cs
@@ -3,7 +3,7 @@ using System;
 namespace Full_GRASP_And_SOLID.Library
 {
     /* La interface IPrinter define la operacion PrintRecipe para imprimir una receta.
-    PrintRecipe es polimorfica porque no pregunta por el destino de impresion.
+    PrintRecipe es polimorfica es utilizada por dos objetos de distinto tipo y no pregunta por el destino de impresion.
     Cumple con el principio de sustitución de Liskov ya que tanto al imprimir por consola
     como al imprimir en un archivo de texto, la receta no se altera*/
     public interface IPrinter
